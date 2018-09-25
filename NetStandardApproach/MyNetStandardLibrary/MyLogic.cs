237ea3df9021e835c2329d5a46e3f8e8e5b79fe6
Andrea Coluccio <ancolucc@microsoft.com>
2018-09-25 17:22:59 +0100

    Initial commit

---
+++ NetStandardApproach/MyNetStandardLibrary/MyLogic.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace MyNetStandardLibrary
+{
+    public class MyLogic
+    {
+        public async Task DoSomething()
+        {
+            //TODO: Add some logic here
+        }
+    }
+}
